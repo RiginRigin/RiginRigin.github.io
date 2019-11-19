f67ab257432fd1d13425335ad85468381e7e5bb3
= <=>
2019-11-19 08:58:36 +0700

    First Commit For PWA

---
+++ precache-manifest.ab52510c40696512e3c221eac6baaa28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951c383ea22685ab2cc7",
+    "url": "/css/about.ed036f12.css"
+  },
+  {
+    "revision": "18d9f9fd44b0f1456297",
+    "url": "/css/chunk-vendors.70084ded.css"
+  },
+  {
+    "revision": "83de1d9432bf02fed3993e73cbd15116",
+    "url": "/index.html"
+  },
+  {
+    "revision": "951c383ea22685ab2cc7",
+    "url": "/js/about.cc2214e9.js"
+  },
+  {
+    "revision": "933ad1530ec216c5d28f",
+    "url": "/js/app.8a5af58a.js"
+  },
+  {
+    "revision": "18d9f9fd44b0f1456297",
+    "url": "/js/chunk-vendors.868c2a51.js"
+  },
+  {
+    "revision": "3abd6f00027864402904e08db79667fb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
